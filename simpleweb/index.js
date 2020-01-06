c293c58a77389ad9a3f4e1b71cfb4bbf837440e3
Usama Ansari <usansa@microsoft.com>
2020-01-06 15:13:24 +0530

    Add simple express app

---
+++ simpleweb/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Bye There');
+})
+
+app.listen(8080, () => {
+    console.log("listening to port 8080");
+})
